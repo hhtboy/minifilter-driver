52e18459281c5b9cb15aa949b16b36c984fdf2c3
Administrator <Administrator@DESKTOP-6RQ5U5K>
2024-11-28 10:34:05 +0900

    초기 커밋.

---
+++ Project3/Project3.c
@@ -0,0 +1,28 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+// Device type
+#define SIOCTL_TYPE 40000
+
+// The IOCTL function codes from 0x800 to 0xFFF are for customer use.
+#define IOCTL_HELLO\
+ CTL_CODE( SIOCTL_TYPE, 0x800, METHOD_BUFFERED, FILE_READ_DATA|FILE_WRITE_DATA)
+
+int main(int argc, char* argv[])
+{
+	HANDLE hDevice;
+	char* welcome = "Hello from userland.";
+	DWORD dwBytesRead = 0;
+	char ReadBuffer[50] = { 0 };
+
+	hDevice = CreateFile(L"\\\\.\\MyDevice", GENERIC_WRITE | GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	printf("Handle : %p\n", hDevice);
+
+	DeviceIoControl(hDevice, IOCTL_HELLO, welcome, strlen(welcome), ReadBuffer, sizeof(ReadBuffer), &dwBytesRead, NULL);
+	printf("Message received from kerneland : %s\n", ReadBuffer);
+	printf("Bytes read : %d\n", dwBytesRead);
+
+	CloseHandle(hDevice);
+
+	return 0;
+}
